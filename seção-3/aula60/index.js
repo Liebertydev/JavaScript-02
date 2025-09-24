65b77bf558d1969e69346efff4e8819101e0cb7e
Lieberty Holanda <lieberty.dev@gmail.com>
2025-09-24 12:10:40 -0300

    Iterando em funções geradoras

--- seção-3/aula60/index.js
+++ seção-3/aula60/index.js
@@ -6,7 +6,13 @@ function* geradora1() {
 
 const g1 = geradora1();
 
-console.log(g1.next());
-console.log(g1.next());
-console.log(g1.next());
-console.log(g1.next());
+// console.log(g1.next().value);
+// console.log(g1.next().value);
+// console.log(g1.next().value);
+// console.log(g1.next().done);
+
+for (let valor of g1) {
+    console.log(valor);
+}
+
+
