b671886dbec1cae7b12cb9ca76ea14c75ea0fd15
Lieberty Holanda <lieberty.dev@gmail.com>
2026-01-28 21:19:47 -0300

    Aprendendo a usar os métodos HTTP no express

---
+++ znode/aula7/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+//         CRIAR   LER   ATUALIZAR  APAGAR
+// CRUD -> CREATE, READ, UPDATE,    DELETE
+//         POST    GET   PUT        DELETE 
+
+// http://meusite.com/ <- GET -> Entregue a página raiz(home)
+// http://meusite.com/sobre <- GET -> Entregue a página sobre
+// http://meusite.com/contato <- GET -> Entregue a página contato
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button>
+        </form>`);
+});
+
+app.post('/', (req, res) => {
+    res.send(`Formulário recebido!`);
+});
+
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato!');
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor Executando na porta 3000');
+});
