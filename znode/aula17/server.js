baf9d03419140d97fbef0ef24ebbb7aad62cf714
Lieberty Holanda <lieberty.dev@gmail.com>
2026-02-16 14:48:35 -0300

    Aprendendo Sessions e flash messages

---
+++ znode/aula17/server.js
@@ -0,0 +1,56 @@
+//Importações
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+//Conexão com o Banco
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        app.emit('Pronto');
+    })
+    .catch(e => console.log(e));
+
+// Configurando as Sessions
+    const session = require('express-session');
+    const MongoStore = require('connect-mongo').default;
+    const flash = require('connect-flash');
+    
+    const sessionOptions = session({
+        secret: process.env.SESSION_SECRET,
+        store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            httpOnly: true
+        }
+    });
+    
+    //Middlewares Globais
+    app.use(sessionOptions);
+    app.use(flash());
+    app.use(express.static(path.resolve(__dirname, 'public')));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(middlewareGlobal);
+    
+    //configs do express
+    app.set('views', path.resolve(__dirname, 'src', 'views'));
+    app.set('view engine', 'ejs');
+    
+    //Rotas
+    app.use(routes);
+
+    //Iniciando server
+    app.on('Pronto', () => {
+        app.listen(3000, () => {
+            console.log('Acessar http://localhost:3000');
+            console.log('Servidor Executando na porta 3000');
+        });
+    });
