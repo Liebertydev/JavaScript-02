a157e3a5635d33596254104836ad4cef0ffd980b
Lieberty Holanda <lieberty.dev@gmail.com>
2026-02-28 14:22:20 -0300

    Aprendemos a logar usuarios e fazer o hash da senha

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res, next) => {
-    res.render('login');
+    if(req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
 exports.register = async function (req, res, next) {
@@ -25,4 +26,42 @@ exports.register = async function (req, res, next) {
         return res.render('404');
     }
     
+};
+
+exports.login = async function (req, res, next) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Login realizado com sucesso.');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('index');
+        });;
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+    
+};
+
+exports.logout = function(req, res, next) {
+    req.session.destroy(err => {
+    if (err) {
+        console.log(err);
+        return res.redirect('/');
+    }
+
+    res.clearCookie('connect.sid');
+    return res.redirect('/');
+});
 }
+
+
--- projetoagenda/src/models/LoginModel.js
+++ projetoagenda/src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -15,17 +16,43 @@ class Login {
         this.user = null;
     }
 
-    async register() {
-        try {
-            this.valida();
-            if (this.errors.length > 0) {
-                return;
-            }
-            this.user = await LoginModel.create(this.body);
-        } catch(e) {
-            console.log(e);
+    async login() {
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        // if(!this.user) {
+        //     this.errors.push('Usuário não existe.')
+        //     return;
+        // }
+
+        if(!this.user || !bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('E-mail ou Senha inválidos.');
+            this.user = null;
+            return;
         }
         
+    }
+
+    async register() {
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        await this.userExists();
+
+        if (this.errors.length > 0) return; // agora checa se houve erro na duplicação do usuario
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+        this.user = await LoginModel.create(this.body);
+    }
+
+    async userExists() {
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if(this.user) this.errors.push('Usuário já Existe.');
     }
 
     valida() {
